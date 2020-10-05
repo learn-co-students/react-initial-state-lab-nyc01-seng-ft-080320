cd14eb9013c34e8fa5fc386ea0fb81ae26e91b10
Russell Sutter <russell.sutter@gmail.com>
2020-10-05 20:11:46 +0000

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -14,7 +14,7 @@ class Bomb extends React.Component {
         if (this.state.secondsLeft === 0) {
             return <div>Boom!</div>
         } else {
-            return <div>{this.state.secondsLeft} seconds left before I go boom</div>
+            return <div>{this.state.secondsLeft} seconds left before I go boom!</div>
         }
     }
 }
