5f885735a63f9a99e9e7e94e9beaabe66b62ef17
stackguardian <mferdous98@aol.com>
2020-10-05 15:53:33 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,53 @@
 // your Bomb code here!
+import React from 'react'
+
+class Bomb extends React.Component{
+    
+    constructor(props) {
+        super()
+        this.state = {
+            secondsLeft: props.initialCount
+        }
+        console.log(props)
+    }
+    
+    render(){
+        
+        return (
+            this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`
+            )
+    }
+  
+}
+
+export default Bomb
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // constructor(props) {
+    //     super()
+
+    //     this.state = {
+    //     secondsLeft: props.initialCount
+    //  }
+    // }
+
+    // render(){
+    //     const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`
+
+    //     return (
+    //         <div>{message}</div>
+    //     )
+    // }
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,51 @@
+import { render } from 'enzyme'
 // your ImageSlider code here!
+import React from 'react'
+
+class ImageSlider extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    render() {
+      return (
+          <div>
+              I am on slide {this.state.currentSlideIndex} 
+          </div>
+      )  
+    }
+
+}
+
+export default ImageSlider
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     constructor() {
+//         super()
+
+//         this.state = {
+//             currentSlideIndex: 0
+//         }
+//     }
+
+// render() {
+//     return(
+//         <h1>I am on slide {this.state.currentSlideIndex}</h1>
+//     )
+//   }
